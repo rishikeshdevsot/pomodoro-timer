a06b962af453ebe47660a9d5b02617af14f82d89
rishikeshdevsot <rishikeshdevsot@gmail.com>
2018-08-30 20:35:44 -0400

    Completed Funtionality: Switching stages, timers for each stage, handling buttons accordingly

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom'
 import './index.css'
 
 const MIL_SEC_25MIN = 25*60*1000;
+const MIL_SEC_5MIN = 5*60*1000;
+const MIL_SEC_15MIN = 15*60*1000;
 
 
 class App extends React.Component{
@@ -10,30 +12,62 @@ class App extends React.Component{
         super(props);
         this.startTime = new Date();
         this.curTime = new Date();
-        this.state = {ctTime : this.startTime.getTime() + MIL_SEC_25MIN  - this.curTime.getTime()};
+        this.startclicked = false;
+        this.interval = MIL_SEC_25MIN;
+        this.state = {ctTime : this.startTime.getTime() + this.interval  - this.curTime.getTime(),
+                       stage: "work",
+                       numWorkStages: 0};
         this.handleReset = this.handleReset.bind(this);
         this.handleStart = this.handleStart.bind(this);
-        this.handleStop = this.handleStop.bind(this);
         this.updateTime = this.updateTime.bind(this);
-    }
-    handleStart(){
-        console.log("clicked Start");
-    }
-    handleStop(){
-        console.log("clicked Stop");
-    }
-    handleReset(){
-        console.log("clicked Reset");
-    }
-    updateTime(){
-        this.curTime = new Date();
-        this.setState({ctTime : this.startTime.getTime() + MIL_SEC_25MIN  - this.curTime.getTime()});
+        this.updateStage = this.updateStage.bind(this);
     }
 
-    componentDidMount(){
-        this.setState({ctTime : this.startTime.getTime() + MIL_SEC_25MIN  - this.curTime.getTime()});
-        setInterval(this.updateTime, 1000);
+    handleStart(){
+        if(!this.startclicked){
+            this.startclicked = true;
+            this.startTime = new Date();
+            this.timeID = setInterval(this.updateTime, 1000);
+        }
     }
+
+    handleReset(){
+        clearInterval(this.timeID);
+        this.startTime = new Date();
+        this.curTime = new Date();
+        this.interval = MIL_SEC_25MIN;
+        this.startclicked = false;
+        this.setState({ctTime : this.startTime.getTime() + this.interval  - this.curTime.getTime(),
+                       stage: "work",
+                       numWorkStages: 0});
+    }
+
+    updateStage(){
+        if((this.state.numWorkStages<4) && (this.state.stage === "work")){
+            this.setState({stage : "sBreak", numWorkStages: this.state.numWorkStages + 1});
+            this.interval = MIL_SEC_5MIN;
+        }
+        else if((this.state.stage === "sBreak")||(this.state.stage === "lBreak")){
+            this.setState({stage : "work"});
+            this.interval = MIL_SEC_25MIN;
+        }
+        else{
+            this.setState({stage : "lBreak", numWorkStages : 0});
+            this.interval = MIL_SEC_15MIN;
+        }
+        this.startTime = new Date();
+        this.curTime = new Date();
+        this.setState({ctTime : this.startTime.getTime() + this.interval  - this.curTime.getTime()})
+    }
+
+    updateTime(){
+        this.curTime = new Date();
+        this.setState({ctTime : this.startTime.getTime() + this.interval  - this.curTime.getTime()});
+        if(this.state.ctTime <= 0){
+            this.updateStage;
+        }
+    }
+
 
     render()
     {
@@ -44,12 +78,12 @@ class App extends React.Component{
                 </div>
                 <div className="Body">
                     <div>
-                        <Timer curTimez={this.state.ctTime}/>
+                        <Timer curTimez={this.state.ctTime} stage={this.state.stage}/>
                     </div>
                     <div className="interaction">
                         <Interaction startClick={this.handleStart} 
-                        stopClick={this.handleStop} 
-                        resetClick={this.handleReset} />
+                        resetClick={this.handleReset} 
+                        skipClick={this.updateStage}/>
                     </div>
                 </div>
             </div>
@@ -58,30 +92,37 @@ class App extends React.Component{
 }
 
 class Timer extends React.Component{
-    constructor(props){
-        super(props);
-    }
+    
     render(){
         this.minutes = Math.floor(this.props.curTimez/(1000*60))
         this.seconds = Math.floor((this.props.curTimez/1000)%60);
+        if(this.props.stage === "lBreak"){
+            this.stage = "Take a break for 15min!"
+        }
+        else if(this.props.stage === "sBreak"){
+            this.stage = "Take a 5min break!"
+        }
+        else{
+            this.stage = "Work for 25min"
+        }
         return(
+        <div>
+        <h1>{this.stage}</h1>
         <p className="displayTime">{String("0"+this.minutes).slice(-2)+":"+String("0"+this.seconds).slice(-2)}</p>
+        </div>
         );
     }
 
 }
 
 class Interaction extends React.Component{
-    constructor(props){
-        super(props);
-    }
 
     render()    {
         return(
             <div>
                 <button onClick = {this.props.startClick}>Start</button>
-                <button onClick = {this.props.stopClick}>Stop</button>
                 <button onClick = {this.props.resetClick}>Reset</button>
+                <button onClick = {this.props.skipClick}>Skip</button>
             </div>
         );
     }
